ba34bc3eaae20011c0e6103a58da62d5ab292dbc
rags <r.raghunandan@gmail.com>
2013-03-20 21:12:57 +0530

    Update args.py

--- args.py
+++ args.py
@@ -1,11 +1,6 @@
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("tasks", help="perform specified task and all it's dependancies",
-                     metavar="task", nargs = '*')
-parser.add_argument('-l', '--list-tasks', help = "List the tasks",
-                     action =  'store_true')
 parser.add_argument('-f', '--file',
-                     help = "Build file to read the tasks from. 'build.py' is default value assumed if this argument is unspecified",
-                     metavar = "file", default =  "build.py")
-parser.parse_args("[--file]") # this line should throw SystemExit
+                     help = "Build file", metavar = "file", default =  "build.py")
+parser.parse_args(["--file"]) # this line should throw SystemExit
